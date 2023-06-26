51160009b311e028ee69441a9f712ec2c3aac2ce
root <root@ip-172-31-40-220.ap-northeast-2.compute.internal>
2023-06-26 17:04:20 +0000

    fix : scripts 파일 수정

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 #환경변수 설정
-export $(cat env/.env | xargs)
+export $(cat ../env/.env | xargs)
 
 #docker이미지 생성
 cd /home/ubuntu/build/scripts
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 
 #run docker
 cd /home/ubuntu/build/scripts
-docker run -d -p 80:8080 test.1.1 test
+docker run -d -p 80:8080 --env-file=../env/.env test.1.1 test
 
