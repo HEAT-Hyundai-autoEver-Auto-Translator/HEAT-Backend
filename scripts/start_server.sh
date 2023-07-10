bfab8e858d853f8cc0b1712a1f8ff5c5a2727587
borish3198 <borish3198@gmail.com>
2023-07-10 18:03:16 +0900

    HEAT-52 fix : 8081포트 실행문제 해결

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@
 #run docker
 cd /home/ubuntu/build/scripts
 docker run -d -p 8080:8080 --env-file=../env/.env test.1.1 test
-docker run -d -p 8081:8081 --env-file=../env/.env test.1.1 test
+docker run -d -p 8081:8081 --env-file=../env/.env test.1.1 test2
