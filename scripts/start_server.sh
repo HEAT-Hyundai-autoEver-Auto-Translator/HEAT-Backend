5938baecddf3fdba04a96bcdce7ded60960c4b3e
borish3198 <borish3198@gmail.com>
2023-07-10 14:24:26 +0900

    HEAT-52 feat : https 설정 및 8081포트 추가

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 
 #run docker
 cd /home/ubuntu/build/scripts
-docker run -d -p 80:8080 --env-file=../env/.env test.1.1 test
-
+docker run -d -p 8080:8080 --env-file=../env/.env test.1.1 test
+docker run -d -p 8081:8081 --env-file=../env/.env test.1.1 test
